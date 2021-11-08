8ad3074be03130ba7349804f351c13958591c24a
Victor Canas <victorcanas972@gmail.com>
2021-11-08 16:17:52 +0100

    added github project

---
+++ Projects/GithubProject/src/test/java/com/victorcanas/GithubProject/GithubProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorcanas.GithubProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

de1a110dc240760b6fc1192e8ea57447be4004bd
Victor Canas <victorcanas972@gmail.com>
2021-11-21 20:49:15 +0100

    adding final spring project

---
+++ Projects/SpringFinalProject/src/test/java/com/victorcanas/SpringFinalProject/SpringFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorcanas.SpringFinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

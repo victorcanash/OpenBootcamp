113d382e68475855b24ebff50e80685e7503e01a
Victor Canas <victorcanas972@gmail.com>
2021-12-21 17:30:13 +0100

    java basico tema 1 ej 1

---
+++ JavaBasico/JavaBasicoTema1Ej1/src/com/victorcanas/JavaBasicoTema1Ej1/Main.java
@@ -0,0 +1,25 @@
+package com.victorcanas.JavaBasicoTema1Ej1;
+
+public class Main {
+
+    public static void main(String[] args){
+        int iVar = 1;
+        System.out.println("Int: " + iVar);
+        byte byVar = 1;
+        System.out.println("Byte: " + byVar);
+        short sVar = 1;
+        System.out.println("Short: " + sVar);
+        long lVar = 1L;
+        System.out.println("Long: " + lVar);
+        float fVar = 1f;
+        System.out.println("Float: " + fVar);
+        double dVar = 1d;
+        System.out.println("Double: " + dVar);
+        boolean bVar = false;
+        System.out.println("Boolean: " + bVar);
+        char cVar = 'h';
+        System.out.println("Char: " + cVar);
+        String strVar = "hello";
+        System.out.println("String: " + strVar);
+    }
+}

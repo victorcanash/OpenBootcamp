8078b6615706c39c79064dd1e2af1557ebbedb74
Victor Canas <victorcanas972@gmail.com>
2022-03-15 20:21:43 +0100

    Sesion 3

---
+++ JavascriptBasico/Sesion3/exercise1.js
@@ -0,0 +1,8 @@
+for (let num = 1; num < 101; num++){
+  if (num % 2 == 0) {
+    console.log(`${num} is even`);
+  } else {
+    console.log(`${num} is odd`);
+  }
+  console.log(num);
+}
---
+++ JavascriptBasico/Sesion3/exercise2.js
@@ -0,0 +1,10 @@
+for (let i = 1; i < 10; i++){
+  if (i == 2 ||
+      i == 3 ||
+      i == 5 ||
+      i == 7){
+    console.log(`Numero primo`);
+  } else {
+    console.log(`${i}`);
+  }
+}

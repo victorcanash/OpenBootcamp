e1716ad69eed0925d20928fb0ebb1636759af0e5
Victor Canas <victorcanas972@gmail.com>
2022-03-16 11:02:37 +0100

    Sesion 4

---
+++ JavascriptBasico/Sesion4/exercise1.js
@@ -0,0 +1,5 @@
+let sentence = "El perro de mi vecina es muy molesto";
+
+sentence.replace("vecina", "vecino");
+
+console.log(sentence);
---
+++ JavascriptBasico/Sesion4/exercise2.js
@@ -0,0 +1,5 @@
+let sentence = "hello world";
+
+sentence.toUpperCase();
+
+console.log(sentence);

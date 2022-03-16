9129fb567d96f03847ae9b340acec8427e40aa05
Victor Canas <victorcanas972@gmail.com>
2022-03-16 17:57:17 +0100

    Sesion 8: Improving ex 2 to complete ex 3

--- JavascriptBasico/Sesion8/exercise2.js
+++ JavascriptBasico/Sesion8/exercise2.js
@@ -1,8 +1,11 @@
 const deleteLast = (list) => {
   if (!list || typeof(list) != 'object'){
     console.log("Bad parameters, insert an array");
+  } else if (list.length <= 0){
+    console.log("Bad parameters, the array must contain at least one element");
+  } else {
+  	list.pop();
   }
-  list.pop();
   return list;
 };
 
